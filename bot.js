9f83cf89ce7ff739795729eb7345f66a12946adc
Tristan Bulmer <63182722+Transit-Lumber@users.noreply.github.com>
2022-11-22 01:54:54 +1300

    Update

--- bot.js
+++ bot.js
@@ -1,15 +1,75 @@
 const discordBotkit = require("botkit-discord");
-const configuration = {
-  token: process.env.DISCORD_TOKEN
+
+const data_config = {
+  token: process.env.DATA_DISCORD_TOKEN
+};
+const minutes_config = {
+  token: process.env.MINUTES_DISCORD_TOKEN
 };
 
-const discordBot = discordBotkit(configuration);
-var normalizedPath = require("path").join(__dirname, "skills");
+const dataBot = discordBotkit(data_config);
+const minutesBot = discordBotkit(minutes_config);
 
-require("fs")
-  .readdirSync(normalizedPath)
-  .forEach(function(file) {
-    require("./skills/" + file)(discordBot);
+require("./skills/data.js")(dataBot);
+require("./skills/minutes.js")(minutesBot);
+module.exports = {
+    dataBot,
+    minutesBot
+}
+
+// Login to minutesBot with discord.js
+// Require discord.js
+const Discord = require("discord.js");
+
+// Create a new client instance
+const client = new Discord.Client( { intents: ["GUILDS", "GUILD_MESSAGES"] } );
+
+// Log in to Discord with your client's token
+client.login(process.env.MINUTES_DISCORD_TOKEN);
+
+// Generate southern greetings
+function genGreeting(plural = true) {
+    var greets = ["howdy", "mornin'", "hello", "hiya", "hey", "good morning"]
+    var subjects = [null, "there"]
+    var users = [null];
+
+    if (plural) {
+        subjects.push("folks");
+        subjects.push("y'all");
+        subjects.push("guys");
+        subjects.push("everybody");
+    }
+    else {
+        subjects.push("partner");
+        subjects.push("friend");
+        subjects.push("pal");
+        subjects.push("buddy");
+        // subjects.push("USERNAME"); // Will be used in DMs in the future
+    }
+
+    // Get the parts of the greeting
+    var g = greets[Math.floor(Math.random() * greets.length)];
+    var s = subjects[Math.floor(Math.random() * subjects.length)];
+    var u = users[Math.floor(Math.random() * users.length)];
+
+    // Check for exceptions
+    // "hiya" can't be used with "y'all", it will just be "hi"
+    if (g == "hiya" && s == "y'all") g = "hi";
+
+    // Generate the greeting
+    var greeting = g + (s ? " " + s : "") + (u ? " " + u : "") + "!";
+
+    // Return the greeting with the first letter capitalized
+    return greeting.charAt(0).toUpperCase() + greeting.slice(1);
+}
+
+// Run when the client is ready
+client.on("ready", () => {
+    console.log("I am ready!");
+
+    // Change the bot's presence
+    client.user.setPresence({ activities: [{ name: "over the Sacred Timelines", type: "WATCHING", details: "" }] });
+
+    // Send a greeting to the channel provided by the api
+    // client.channels.cache.get(process.env.MINUTES_ID).send(genGreeting());
   });
-
-module.exports = discordBot;
--- server.js
+++ server.js
@@ -1,12 +1,236 @@
-const express = require("express");
+var checkdate = new Date("August 30 2022 18:13");
+var eventname = "Something Boring"
+var timezoneoffset = 12 * 1000 * 3600
+
 const fs = require("fs");
+const express = require("express");
 const discordBotkit = require("botkit-discord");
-const puns = require("puns.dev");
-var Client = require("uptime-robot");
-const app = express();
 const discordBot = require("./bot");
-app.use(require("./guides"));
+const app = express();
+const fetchUrl = require("fetch").fetchUrl;
+
+var gifSent = false
+var gifQueries = [
+    {
+        "query": "dua lipa",
+        "weight": 3
+    },
+    {
+        "query": "jess bush star trek nurse christine chapel",
+        "weight": 5
+    },
+    {
+        "query": "death on the nile emma mackey jacqueline de bellefort",
+        "weight": 6
+    },
+    {
+        "query": "sylvie sophia di martino",
+        "weight": 8
+    }
+]
+
+// var faces = [":smiley:", ":grin:", ":blush:", ":innocent:", ":smiling_face_with_3_hearts:", ":heart_eyes:", ":star_struck:", ":kissing_heart:", ":relaxed:", ":kissing_closed_eyes:", ":hugging:", ":drooling_face:", ":hot_face:", ":sunglasses:", ":flushed:", ":pleading_face:", ":scream:", ":smiling_imp:"]
+var faces = [""] // Temporary
+var intervals = ["reset", "80 days", "69 days", "60 days", "50 days", "40 days", "30 days", "21 days", "14 days", "7 days", "5 days", "3 days", "2 days", "24 hours", "12 hours", "8 hours", "5 hours", "3 hours", "2 hours", "60 minutes", "30 minutes", "10 minutes", "5 minutes", "0 minutes"]
+
+try {
+    var update_stage = fs.readFileSync("./time_status.txt", "utf8");
+    var currentpos = intervals.indexOf(update_stage) + 1
+}
+catch (err) {
+    var currentpos = 0;
+}
+
+var intervalID = setInterval(timecheck, 10000); // Every 10 seconds, check the coundown
+var gifLoop = setInterval(checkGIF, 40000); // Every 40 seconds, check if a gif should be sent
+// var repoCheck = setInterval(checkRepo, 60000); // Every minute, check if the repo's submodules need to be updated
+
+function timecheck() {
+    var currentdate = new Date();
+    var diff = checkdate.getTime() - currentdate.getTime() - timezoneoffset;
+
+    var daydiff = Math.ceil(diff / (1000 * 3600 * 24))
+    var hourdiff = Math.ceil(diff / (1000 * 3600))
+    var mindiff = Math.ceil(diff / (1000 * 60))
+
+    var diffmessage = (daydiff + " days")
+
+    if (daydiff < 2) {
+        if (hourdiff < 2) {
+            diffmessage = (mindiff + " minutes")
+        }
+        else {
+            diffmessage = (hourdiff + " hours")
+        }
+    }
+
+    if (diffmessage == intervals[currentpos]) {
+        if (diffmessage == "0 minutes") {
+            sendMessage(`${eventname} time ${faces[Math.round(Math.floor(Math.random() * faces.length))]}`, "MEDIA")
+        }
+        else {
+            sendMessage(`${diffmessage} until ${eventname} ${faces[Math.round(Math.floor(Math.random() * faces.length))]}`, "MEDIA")
+        }
+        currentpos += 1
+        fs.writeFile('./time_status.txt', diffmessage, function (err) {
+            if (err) sendMessage("Error message received from the JS Server:\n`" + err + "`", "MEDIA");
+        });
+    }
+}
+
+function sendMessage(message, hookname) {
+    console.log("Sending message \"" + message + "\" to " + hookname + " webhook")
+    if (message == null || message.lenght < 1) {
+        return
+    }
+
+    if (hookname == "MEDIA") {
+        var id = process.env.MEDIA_ID;
+        var token = process.env.MEDIA_TOKEN;
+    }
+    else if (hookname == "GIF") {
+        var id = process.env.GIF_ID;
+        var token = process.env.GIF_TOKEN;
+    }
+    else if (hookname == "TEST") {
+        var id = process.env.TEST_ID;
+        var token = process.env.TEST_TOKEN;
+    }
+
+    const Discord = require("discord.js");
+    const webhook = new Discord.WebhookClient({
+        id: id,
+        token: token
+    })
+
+    console.log(webhook)
+
+    webhook.send({
+        content: message
+    }).catch(console.error);
+}
+
 app.use(express.static("public"));
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+app.get("/wakeup", function (request, response) {
+    response.send("Wakeup successful");
+    console.log("Pinged")
 });
+app.get("/notify", function (request, response) {
+    // Send a message to the req
+    var message = request.query.message
+    var hook = request.query.hook
+    sendMessage(message, hook)
+    response.send("Message sent")
+});
+app.get("/update", function (request, response) {
+    // Update the repo using github dev (using puppeteer)
+    // @ https://github.dev/Transit-Lumber/Transit-Lumber.github.io
+
+    // Use the link above and send a screenshot to a chat
+
+    var url = "https://github.dev/Transit-Lumber/Transit-Lumber.github.io/"
+
+    puppeteer.launch().then(async browser => {
+      const page = await browser.newPage();
+    
+      await page.goto(url)
+
+      // Wait for the page to load and take a screenshot
+      await page.waitForSelector("html")
+
+      await page.screenshot({path: 'screenshot.png'});
+
+      // Send the screenshot from this repo and a message to the test channel
+      sendMessage("Screenshot taken", "TEST")
+      sendMessage(__dirname + "/screenshot.png", "TEST")
+
+      await browser.close();
+    })();
+});
+
+const listener = app.listen(process.env.PORT, function () {
+    console.log("Your app is listening on port " + listener.address().port);
+});
+
+function getGif() {
+    // Get a random number between 0 and 8
+    var random = Math.floor(Math.random() * 8)
+
+    // Get a random query from the array based on the random number
+    var query = gifQueries.find(x => random <= x.weight - 1).query
+
+    // Use fetch and the Giphy API to get a random gif
+    var url = "https://tenor.googleapis.com/v2/search?q=" + query + "&key=" + process.env.TENOR_KEY + "&client_key=gif_bot&limit=1&random=true"
+    var response = fetchUrl(url, function (error, meta, body) {
+        var data = JSON.parse(body.toString())
+        sendMessage(data.results[0].url, "GIF")
+    })
+}
+
+function checkGIF() {
+    if (gifSent) {
+        gifSent = false
+        return
+    }
+
+    var currenttime = new Date();
+
+    console.log("Checking for gif, current time is " + currenttime.getHours() + ":" + currenttime.getMinutes())
+
+    // Send a gif every 2 hours from 7am till 1am
+    if (![18, 20, 22, 0, 2, 4, 6, 8, 10, 12].includes(currenttime.getHours())) {
+        return
+    }
+
+    if (currenttime.getMinutes() == 0) {
+        console.log("Time matches, sending gif")
+        getGif()
+        gifSent = true
+    }
+}
+
+function checkRepo() {
+    // Get the list of submodules from the repo
+    var url = "https://raw.githubusercontent.com/Transit-Lumber/Transit-Lumber.github.io/main/.gitmodules"
+    var response = fetchUrl(url, function (error, meta, body) {
+        // Get the list of submodules from the repo
+        var data = body.toString().split("\"")
+
+        // Get the commit hashs of the submodules
+        var url = "https://api.github.com/repos/Transit-Lumber/Transit-Lumber.github.io/git/trees/main"
+
+        // Run a fetch on the url
+        var response = fetchUrl(url, function (error, meta, body) {
+            // Get the data from the fetch that are submodules
+            var hashs = JSON.parse(body.toString()).tree.filter(x => data.includes(x.path)).map(x => x.sha)
+
+            // Loop through the submodules
+            for (var i = 0; i < hashs.length; i++) {
+                var hash = hashs[i]
+                var url = "https://api.github.com/repos/Transit-Lumber/" + data[i * 2 + 1] + "/git/trees/main"
+
+                // Run a fetch on the url (wait continue when done)
+                matchHashes(hash, url)
+            }
+        })
+    })
+}
+
+checkRepo()
+
+async function matchHashes(hash, url) {
+    // Run a fetch on the url
+    var response = await fetchUrl(url, function (error, meta, body) {
+        var data = JSON.parse(body.toString()).sha
+
+        // Check if the submodule has been updated
+        if (hash != data) {
+            // Update the submodule
+            updateSubmodule(url.split("/")[5])
+        }
+    })
+}
+
+function updateSubmodule(name) {
+    console.log("Updating submodule " + name)
+}
