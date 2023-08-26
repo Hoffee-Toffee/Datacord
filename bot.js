ddcc377990d0a5cf970a7424caa0f92d9b143e63
Hoffee-Toffee <Tristan-Bulmer@live.nmit.ac.nz>
2023-08-27 01:17:51 +1200

    Blacklist barebones test

--- bot.js
+++ bot.js
@@ -5,18 +5,11 @@ const test_config = {
   token: "MTE0NDkwMTA0MDQ5MjI1NzMxMA.G6E4td.2OhiWPjA8lMQenw70q_Oz6ayBiFuoODyymQycY"
 };
 
-const testBot = discordBotkit(test_config);
-
-require("./skills/test.js")(testBot);
-module.exports = {
-  testBot,
-}
-
 const timezoneoffset = 12 * 60 * 60 * 1000; // 12 hours in milliseconds
 
 // Login to minutesBot and dataBot with discord.js
 // Require discord.js
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Message } = require('discord.js');
 const { time } = require("console");
 
 // Create the new clients instances including the intents needed for the bots like presence and guild messages
@@ -24,13 +17,27 @@ const testClient = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildPresences
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.MessageContent
   ]
 });
 
 // Log in to Discord with your clients tokens
 testClient.login(test_config.token)
 
-testClient.on("ready", () => {
-  // ...
+testClient.on("messageCreate", (message = new Message()) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (message.author.bot) return;
+    
+  if ([".blacklist", ".bl"].includes(message.content.toLowerCase())) {
+    message.channel.messages.fetch(message.reference.messageId)
+    .then(msg => {
+      console.log(msg)
+      if (msg.author.id == "GIF BOT ID") {
+        // Add {msg.content} to blacklist
+        // Send message stating the update
+      }
+    })
+    .catch(console.error);
+  }
 });
