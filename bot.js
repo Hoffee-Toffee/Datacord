436ab4229afb040688274f1ea10c6bc392aed325
Hoffee-Toffee <Tristan-Bulmer@live.nmit.ac.nz>
2023-08-28 16:55:02 +1200

    Editing out of session test

--- bot.js
+++ bot.js
@@ -1,16 +1,20 @@
-const discordBotkit = require("botkit-discord");
-const fs = require("fs");
+const discordBotkit = require('botkit-discord')
+const fs = require('fs')
+
+var messageID = '1145580607372533821'
 
 const test_config = {
-  token: "MTE0NDkwMTA0MDQ5MjI1NzMxMA.G6E4td.2OhiWPjA8lMQenw70q_Oz6ayBiFuoODyymQycY"
-};
+  token:
+    'MTE0NDkwMTA0MDQ5MjI1NzMxMA.G6E4td.2OhiWPjA8lMQenw70q_Oz6ayBiFuoODyymQycY',
+}
 
-const timezoneoffset = 12 * 60 * 60 * 1000; // 12 hours in milliseconds
+const timezoneoffset = 12 * 60 * 60 * 1000 // 12 hours in milliseconds
 
 // Login to minutesBot and dataBot with discord.js
 // Require discord.js
-const { Client, GatewayIntentBits, Message } = require('discord.js');
-const { time } = require("console");
+const { Client, GatewayIntentBits, Message } = require('discord.js')
+const { time } = require('console')
+const { channel } = require('diagnostics_channel')
 
 // Create the new clients instances including the intents needed for the bots like presence and guild messages
 const testClient = new Client({
@@ -18,26 +22,52 @@ const testClient = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.MessageContent
-  ]
-});
+    GatewayIntentBits.MessageContent,
+  ],
+})
 
 // Log in to Discord with your clients tokens
 testClient.login(test_config.token)
 
-testClient.on("messageCreate", (message = new Message()) => {
+testClient.on('messageCreate', async (message = new Message()) => {
   // Exit and stop if the prefix is not there or if user is a bot
-  if (message.author.bot) return;
-    
-  if ([".blacklist", ".bl"].includes(message.content.toLowerCase())) {
-    message.channel.messages.fetch(message.reference.messageId)
-    .then(msg => {
-      console.log(msg)
-      if (msg.author.id == "GIF BOT ID") {
-        // Add {msg.content} to blacklist
-        // Send message stating the update
-      }
-    })
-    .catch(console.error);
+  if (message.author.bot) return
+
+  if (message.content.toLowerCase() == '.') {
+    message.channel.send(
+      message.guild.members.cache.get(message.author.id).nickname ||
+        message.author.globalName
+    )
   }
-});
+
+  if (message.content.toLowerCase().startsWith('.update')) {
+    console.log(message.content.slice(8))
+    await message.channel.messages
+      .get(
+        message.content.toLowerCase().startsWith('.update ')
+          ? message.content.slice(8)
+          : messageID
+      )
+      .edit(`Updated at ${new Date().toLocaleTimeString()}!`)
+  }
+
+  if (
+    ['.kill', '.stop', '.end', '.exit'].includes(message.content.toLowerCase())
+  ) {
+    message.channel.send('Ended bot.').finally(() => {
+      process.exit()
+    })
+  }
+})
+
+testClient.on('ready', async () => {
+  await testClient.channels
+    .fetch('1145523077443235962')
+    .then((channel) =>
+      channel.messages
+        .fetch(messageID)
+        .then((message) =>
+          message.edit(`Updated at ${new Date().toLocaleTimeString()}!`)
+        )
+    )
+})
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 // require('dotenv').config();
 var timezoneoffset = 12 * 1000 * 3600
 
-const fs = require("fs");
-const express = require("express");
-const discordBotkit = require("botkit-discord");
-const app = express();
-const fetchUrl = require("fetch").fetchUrl;
+const fs = require('fs')
+const express = require('express')
+const discordBotkit = require('botkit-discord')
+const app = express()
+const fetchUrl = require('fetch').fetchUrl
 
-const discordBot = require("./bot");
+const discordBot = require('./bot')
 
-app.use(express.static("public"));
-app.get("/wakeup", function (request, response) {
-    response.send("Wakeup successful.")
-    console.log(`Pinged at ${new Date()}`)
-});
+app.use(express.static('public'))
+app.get('/wakeup', function (request, response) {
+  response.send('Wakeup successful.')
+  console.log(`Pinged at ${new Date()}`)
+})
 
 const listener = app.listen(process.env.PORT, function () {
-    console.log("Your app is listening on port " + listener.address().port);
-});
-
-var kill = setTimeout(() => {
-    process.exit()
-}, 1000 * 60)
+  console.log('Your app is listening on port ' + listener.address().port)
+})
