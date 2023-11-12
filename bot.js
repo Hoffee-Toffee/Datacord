520182b4cf763d9e6b0926dab0961d6bd1cc5e70
Hoffee-Toffee <Tristan-Bulmer@live.nmit.ac.nz>
2023-11-12 14:45:56 +1300

    Fix

--- bot.js
+++ bot.js
@@ -889,7 +889,7 @@ async function sendReport(client, time) {
 
 minutesClient.on('messageReactionAdd', async (reaction, user) => {
   // Get the message reacted to
-  minutesClient.channels.cache.get(message.channel.id).messages
+  message.channel.messages
     .fetch(message.reference.messageId)
     .then((msg) => {
       console.log(msg)
