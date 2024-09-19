eda5280a26db2292295c342df3af0301707887e5
Hoffee Toffee <tb59391@gmail.com>
2024-09-19 13:10:45 +1200

    Audio stream fix attempt 5

--- bots/autoAmb.js
+++ bots/autoAmb.js
@@ -499,7 +499,7 @@ async function startStream(testing = false) {
         '-s 1280x720',
         '-r 30',
         '-preset ultrafast',
-        '-max_alloc 1024M',
+        '-max_alloc 8192',
       ])
       .output(fullStreamURL)
       .on('start', () => {
